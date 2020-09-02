2ca90bfd79448ff72b8f92e76e1ef697e11134e0
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-02 15:37:49 +0100

    scripts: Add build script to facilitate quick iteration
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ scripts/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+set -x
+
+MODE="debug"
+# COMPILER="dmd"
+COMPILER="ldc2"
+
+if [[ ! -z "$1" ]]; then
+    MODE="$1"
+fi
+
+function buildProject()
+{
+    dub build --parallel -b "${MODE}" --compiler="${COMPILER}" --skip-registry=all -v
+}
+
+if [[ "$MODE" == "release" ]]; then
+    COMPILER="ldc2"
+    buildProject
+    strip bin/moss
+elif [[ "$MODE" == "optimized" ]]; then
+    MODE="release"
+    buildProject
+elif [[ "$MODE" == "debug" ]]; then
+    buildProject
+else
+    echo "Unknown build mode"
+    exit 1
+fi
+
