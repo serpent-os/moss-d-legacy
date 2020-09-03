0dc55a327b6a8375a7eb434ede7c1e4ff6236c83
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-03 13:26:05 +0100

    cli/search: Fix sed issue
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- source/moss/cli/search.d
+++ source/moss/cli/search.d
@@ -17,7 +17,7 @@
  *    appreciated but is not required.
  * 2. Altered source versions must be plainly marked as such, and must not be
  *    misrepresented as being the original software.
- * 3. This notice may not be searchd or altered from any source distribution.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 /**
