787fc6b526843a81888af07177c94e8161686af7
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-11 16:56:05 +0100

    binary: Introduce record notion.
    This is essentially the key used in our binary datum store.
    It has no value itself and is just used to encode metadata
    on the value.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ source/moss/format/binary/record.d
@@ -0,0 +1,79 @@
+/*
+ * This file is part of moss.
+ *
+ * Copyright © 2020 Serpent OS Developers
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+module moss.format.binary.record;
+
+import std.stdint;
+import moss.format.binary.endianness;
+
+/**
+ * The type of record encountered.
+ * We limit this to a small selection of predefined data types.
+ */
+enum RecordType : uint8_t
+{
+    Unknown = 0,
+    Int8,
+    Uint8,
+    Int16,
+    Uint16,
+    Int32,
+    Uint32,
+    Int64,
+    Uint64,
+    Binary,
+    String
+}
+
+/**
+ * We support a predefined set of record types which are additionally
+ * tagged for their type.
+ */
+enum RecordTag : uint16_t
+{
+    Unknown = 0,
+    Name, /** Name of the package */
+    Architecture, /** Architecture of the package */
+    Version, /** Version of the package */
+    Sumary, /** Summary of the package */
+    Description, /** Description of the package */
+};
+
+/**
+ * Records are found in each moss package after the initial header.
+ * They contain all meta-information on the package and are variable
+ * length in nature.
+ *
+ * To skip all records requires skipping the length of every record
+ * encountered. The payload will then be encountered before the final 0
+ * byte.
+ */
+struct Record
+{
+    uint16_t length; /** 2 bytes per record length*/
+    RecordTag tag; /** 2 bytes for the tag */
+    RecordType type; /** 1 byte for the type */
+    ubyte[2] padding;
+};
+
+static assert(Record.sizeof == 8,
+        "Record size must be 8 bytes, not " ~ Record.sizeof.stringof ~ " bytes");
