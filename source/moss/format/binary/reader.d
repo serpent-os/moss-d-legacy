735db1c9fcb650856429fc7c797f34425162c965
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-09-17 16:00:58 +0100

    binary: Begin a new Reader type
    The Reader will be used to provide a low-level mechanism with which to
    read moss binary packages and parse them.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ source/moss/format/binary/reader.d
@@ -0,0 +1,66 @@
+/*
+ * This file is part of moss.
+ *
+ * Copyright © 2020 Serpent OS Developers
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+module moss.format.binary.reader;
+
+public import std.stdio : File;
+import moss.format.binary.header;
+
+/**
+ * The Reader is a low-level mechanism for parsing Moss binary packages.
+ */
+struct Reader
+{
+
+private:
+
+    File _file;
+    Header _header;
+
+public:
+    @disable this();
+
+    /**
+     * Construct a new Reader for the given filename
+     */
+    this(File file) @trusted
+    {
+        _file = file;
+    }
+
+    ~this() @safe
+    {
+        close();
+    }
+
+    /**
+     * Flush and close the underying file.
+     */
+    final void close() @safe
+    {
+        if (!_file.isOpen())
+        {
+            return;
+        }
+        _file.close();
+    }
+}
