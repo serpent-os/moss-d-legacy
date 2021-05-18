8b861060bfe4ebbd5b00a0540f7b52e530b1c713
Ikey Doherty <ikey@serpentos.com>
2021-05-18 22:46:48 +0100

    db/state/entries: Add needed enums for State recording
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/db/state/entries.d
+++ source/moss/db/state/entries.d
@@ -36,6 +36,39 @@ import moss.context;
 const uint16_t stateEntriesPayloadVersion = 1;
 
 /**
+ * A selection can be either binary or source, changing how we resolve
+ * the selection
+ */
+public enum SelectionType : uint8_t
+{
+    Source = 0,
+    Binary = 1,
+}
+
+/**
+ * Each selection can have specific flags set on it which allow us to
+ * understand whether something was explicitly installed (UserInstalled)
+ * or appeared through use of the depsolver
+ */
+public enum SelectionFlags : uint32_t
+{
+    /** No custom policy is applied */
+    DefaultPolicy = 1 << 0,
+
+    /** The user explicitly installed this selection */
+    UserInstalled = 1 << 1,
+
+    /** Installed implicitly through the depsolver */
+    DepInstalled = 1 << 2,
+
+    /** Never seek an update for this package  */
+    Hold = 1 << 3,
+
+    /** Always prefer source for this package */
+    PreferSource = 1 << 4,
+}
+
+/**
  * The EntryRelationalKey maps each entry in the StateEntriesDB to a state
  * identifier specified in the StateMetaDB
  */
@@ -43,6 +76,22 @@ const uint16_t stateEntriesPayloadVersion = 1;
 {
     /** Unique ID for the corresponding state */
     @AutoEndian uint64_t stateID = 0;
+}
+
+/**
+ * Stores the selection information in the ECS
+ */
+@serpentComponent public struct SelectionComponent
+{
+    /**
+     * The type of selection, i.e. binary, source
+     */
+    SelectionType type;
+
+    /**
+     * The flags on this selection
+     */
+    SelectionFlags flags;
 }
 
 /**
@@ -60,6 +109,7 @@ public final class StateEntriesDB : MossDB
     {
         super(entityManager);
         entityManager.tryRegisterComponent!EntryRelationalKey;
+        entityManager.tryRegisterComponent!SelectionComponent;
         filePath = context.paths.db.buildPath("state.entries.db");
     }
 
