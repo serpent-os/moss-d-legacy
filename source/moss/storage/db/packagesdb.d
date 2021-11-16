89c91018e927721d018ec6b4478fc571ae88e5e9
Ikey Doherty <ikey@serpentos.com>
2021-11-16 01:45:23 +0000

    db/packagesdb: Remove explicit handling of DB for now
    We're in the process of "elegantly" (..) rebasing installdb into
    SystemPackagesDB on top of our lovely new MetaDB. Realistically
    we just need to allow subclassing and ensure some final methods
    are available.
    
    Remember, DBs aren't plugins, ever. Our plugins must encapsulate
    the DB.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/storage/db/packagesdb.d
+++ source/moss/storage/db/packagesdb.d
@@ -39,7 +39,7 @@ public import moss.deps.registry.plugin;
  * candidates to provide a system level of resolution for packages no longer referenced
  * from a repository.
  */
-public final class SystemPackagesDB : RegistryPlugin
+public final class SystemPackagesDB
 {
     /**
      * Construct a new SystemPackagesDB which will immediately force a reload of the
@@ -93,144 +93,14 @@ public final class SystemPackagesDB : RegistryPlugin
      */
     string installPayload(MetaPayload payload)
     {
-        auto pkgID = payload.getPkgID();
-        enforce(pkgID !is null, "installPayload(): Unable to get pkgID");
-
-        auto metabucket = db.bucket(metadataBucket(pkgID));
-
-        const auto result = db.bucket("index").get!string(pkgID);
-        if (result.found)
-        {
-            /* Already stored this asset. */
-            return pkgID;
-        }
-
-        /* Store this in the index now */
-        db.bucket("index").set(pkgID, metadataBucket(pkgID));
-
-        void joinRecord(V)(const(string) keyName, RecordType expType, RecordType realType, in V value)
-        {
-            enforce(expType == realType,
-                    "installPayload(): Type should be %s, not %s".format(expType, realType));
-            metabucket.set(keyName, value);
-        }
-
-        /* Walk through and retrieve all supported values */
-        foreach (record; payload)
-        {
-            switch (record.tag)
-            {
-            case RecordTag.Architecture:
-                joinRecord("architecture",
-                        RecordType.String, record.type, record.get!string);
-                break;
-            case RecordTag.BuildRelease:
-                joinRecord("buildRelease",
-                        RecordType.Uint64, record.type, record.get!uint64_t);
-                break;
-            case RecordTag.Description:
-                joinRecord("description",
-                        RecordType.String, record.type, record.get!string);
-                break;
-            case RecordTag.Homepage:
-                joinRecord("homepage", RecordType.String,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.Name:
-                joinRecord("name", RecordType.String,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.Release:
-                joinRecord("release", RecordType.Uint64,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.SourceID:
-                joinRecord("sourceID", RecordType.String,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.Summary:
-                joinRecord("summary", RecordType.String,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.Version:
-                joinRecord("version", RecordType.String,
-                        record.type, record.get!string);
-                break;
-            case RecordTag.Unknown:
-            default:
-                break;
-            }
-        }
-
-        /* When we know the pkg will be definitely used, bump to 1. Start at 0 */
-        setRefCount(pkgID, 0);
-        return pkgID;
-    }
-
-    override RegistryItem[] queryProviders(in ProviderType type, in string matcher)
-    {
-        return [];
-    }
-
-    /**
-     * Search our local DB for a match to the pkgID
-     */
-    override Nullable!RegistryItem queryID(const(string) pkgID)
-    {
-        Nullable!RegistryItem ret = Nullable!RegistryItem(RegistryItem.init);
-
-        immutable auto bucketID = db.bucket("index").get!string(pkgID);
-
-        /* Set if exists */
-        if (bucketID.found)
-        {
-            ret = RegistryItem(pkgID, this);
-        }
-
-        return ret;
-    }
-
-    override const(Dependency)[] dependencies(in string pkgID) const
-    {
-        return null;
-    }
-
-    override const(Provider)[] providers(in string pkgID) const
-    {
         return null;
     }
 
     /**
      * Set an explicit refCount for the pkgID
      */
-    void setRefCount(const(string) pkgID, uint64_t refCount)
-    {
-        db.bucket(metadataBucket(pkgID)).set("refCount", refCount);
-    }
-
-    /**
-     * Get current refcount for the pkg
-     */
-    uint64_t getRefCount(const(string) pkgID)
-    {
-        auto result = db.bucket(metadataBucket(pkgID)).get!uint64_t("refCount");
-        if (!result.found)
-        {
-            return 0;
-        }
-
-        return result.value;
-    }
 
 private:
-
-    /**
-     * Return the per package metadata bucket name
-     */
-    pragma(inline, true) string metadataBucket(const(string) pkgID)
-    {
-        return "metadata.%s".format(pkgID);
-    }
 
     Database db = null;
 }
