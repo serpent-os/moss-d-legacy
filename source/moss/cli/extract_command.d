ac106ec34cfd1f919d59da677ce990773a1d797c
Rune Morling <ermo@serpentos.com>
2022-06-24 04:33:11 +0200

    [NFC] Update ddoc headers for REUSE license compliance
    Signed-off-by: Rune Morling <ermo@serpentos.com>

--- source/moss/cli/extract_command.d
+++ source/moss/cli/extract_command.d
@@ -1,23 +1,16 @@
 /*
- * This file is part of moss.
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
  *
- * Copyright © 2020-2021 Serpent OS Developers
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.cli.extract_command
  *
- * This software is provided 'as-is', without any express or implied
- * warranty. In no event will the authors be held liable for any damages
- * arising from the use of this software.
+ * Extract moss .stone package contents without installation.
  *
- * Permission is granted to anyone to use this software for any purpose,
- * including commercial applications, and to alter it and redistribute it
- * freely, subject to the following restrictions:
- *
- * 1. The origin of this software must not be misrepresented; you must not
- *    claim that you wrote the original software. If you use this software
- *    in a product, an acknowledgment in the product documentation would be
- *    appreciated but is not required.
- * 2. Altered source versions must be plainly marked as such, and must not be
- *    misrepresented as being the original software.
- * 3. This notice may not be removed or altered from any source distribution.
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
  */
 
 module moss.cli.extract_command;
