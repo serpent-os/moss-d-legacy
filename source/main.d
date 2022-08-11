c4ce6b62955f00f2e8ff516ae1c5de0f5b880d19
Ikey Doherty <ikey@serpentos.com>
2022-08-11 14:49:40 +0100

    It'll be worth it shush
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/main.d
+++ source/main.d
@@ -7,32 +7,17 @@
 /**
  * main
  *
- * Main entry point for moss. Parse cmd-line options and set up logging.
+ * Main executable for `moss`
  *
  * Authors: Copyright © 2020-2022 Serpent OS Developers
  * License: Zlib
  */
+
 module main;
 
-import std.stdio;
-import moss.cli;
+import std.experimental.logger;
 
 int main(string[] args)
 {
-    auto clip = cliProcessor!MossCLI(args);
-    auto add = clip.addCommand!AddCommand;
-    add.addCommand!AddRepoCommand;
-    clip.addCommand!ExtractCommand;
-    clip.addCommand!InfoCommand;
-    clip.addCommand!InspectCommand;
-    clip.addCommand!IndexCommand;
-    clip.addCommand!InstallCommand;
-    auto ls = clip.addCommand!ListCommand;
-    ls.addCommand!ListAvailableCommand;
-    ls.addCommand!ListInstalledCommand;
-    clip.addCommand!RemoveCommand;
-    clip.addCommand!UpdateCommand;
-    clip.addCommand!VersionCommand;
-    clip.addCommand!HelpCommand;
-    return clip.process(args);
+    return 0;
 }
