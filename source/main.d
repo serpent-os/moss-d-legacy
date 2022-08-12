292ffc9f35224d12aa7d88ce44d6a08825fbb307
Ikey Doherty <ikey@serpentos.com>
2022-08-12 23:09:57 +0100

    client: Stop using Registry until its @safe
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/main.d
+++ source/main.d
@@ -19,8 +19,19 @@ import std.experimental.logger;
 
 import moss.client;
 
+/**
+ * Main routine.
+ *
+ * Params:
+ *      args = Runtime arguments
+ * Returns: 0 if successful
+ */
 int main(string[] args) @safe
 {
     auto client = new MossClient();
+    scope (exit)
+    {
+        client.close();
+    }
     return 0;
 }
