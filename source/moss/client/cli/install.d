a70afdaccd9ea61bc484d367f7622c2bee37cbb9
Ikey Doherty <ikey@serpentos.com>
2022-08-24 01:05:01 +0100

    install: Add dep solving operations but don't install just yet
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/client/cli/install.d
+++ source/moss/client/cli/install.d
@@ -18,6 +18,13 @@ module moss.client.cli.install;
 public import moss.core.cli;
 
 import moss.client.cli : initialiseClient;
+import moss.deps.registry;
+import std.stdio : writefln, writef;
+import std.experimental.logger;
+import std.range : empty;
+import std.string : join, wrap, format;
+import std.algorithm : map;
+import moss.client.ui;
 
 /**
  * Primary grouping for the moss cli
@@ -38,6 +45,66 @@ import moss.client.cli : initialiseClient;
         {
             cl.close();
         }
+
+        Transaction tx = cl.registry.transaction();
+        RegistryItem[] selections;
+        foreach (item; argv)
+        {
+            auto candidates = cl.registry.byName(item);
+            if (candidates.empty)
+            {
+                errorf("Cannot find package %s", item);
+                return 1;
+            }
+            auto chosen = candidates.front;
+            () @trusted { tracef("Picking: %s", chosen); }();
+            selections ~= chosen;
+        }
+        tx.installPackages(selections);
+        auto result = tx.apply();
+        auto problems = tx.problems();
+        if (!problems.empty)
+        {
+            cl.ui.warn("Unable to install due to the following problems...");
+            foreach (problem; problems)
+            {
+                cl.ui.inform!"[%s] %s"(problem.type, problem.item);
+            }
+        }
+        cl.ui.inform("The following packages will be installed\n");
+        ulong colWritten = 0;
+        ulong elemWritten = 0;
+        foreach (r; result)
+        {
+            if (r.installed)
+            {
+                continue;
+            }
+            auto writeLen = r.info.name.length + r.info.versionID.length + 1;
+            auto toWrite = format!"%s %s"(Text(r.info.name)
+                    .attr(Attribute.Bold), Text(r.info.versionID).fg(Color.Magenta));
+            if (writeLen + colWritten > 64)
+            {
+                colWritten = 0;
+                writef!"\n %s"(toWrite);
+            }
+            else
+            {
+                if (elemWritten == 0)
+                {
+                    writef!" %s"(toWrite);
+                    colWritten += writeLen;
+                }
+                else
+                {
+                    writef!", %s"(toWrite);
+                    colWritten += writeLen + 2;
+                }
+            }
+            ++elemWritten;
+        }
+        writef("\n");
+
         return 0;
     }
 }
