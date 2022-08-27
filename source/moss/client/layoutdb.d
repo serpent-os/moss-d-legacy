069ef93a3c90b0702e62df659d5f1624a0186aad
Ikey Doherty <ikey@serpentos.com>
2022-08-27 11:07:05 +0100

    layoutdb: Start work on new ORM driven LayoutDB
    We abuse the primary key to get the old behaviour of buckets of
    entries, so that we can "remember" where a package's files are
    kept.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/client/layoutdb.d
@@ -0,0 +1,114 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.client.layoutdb
+ *
+ * Layout Database
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.client.layoutdb;
+
+import moss.client.installation;
+import moss.db.keyvalue;
+import moss.db.keyvalue.errors;
+import moss.db.keyvalue.interfaces;
+import moss.db.keyvalue.orm;
+import std.exception : enforce;
+import std.experimental.logger;
+import std.file : exists;
+import std.string : format;
+public import moss.format.binary.payload.layout;
+
+public import moss.core.errors;
+
+public alias LayoutResult = Optional!(Success, Failure);
+
+/**
+ * A Layout is keyed by a unique package ID and
+ * contains all entries
+ */
+public @Model struct Layout
+{
+    /**
+     * Unique package ID
+     */
+    @PrimaryKey string pkgID;
+
+    /**
+     * Entries for the filesystem
+     */
+    LayoutEntry entries;
+}
+
+/**
+ * Manages our Layout entries
+ */
+public final class LayoutDB
+{
+    @disable this();
+
+package:
+
+    /**
+     * Construct a new LayoutDB from the given install
+     */
+    this(Installation install) @safe
+    {
+        this.installation = install;
+    }
+
+    /**
+     * Attempt to connect
+     */
+    LayoutResult connect() @safe
+    {
+        immutable dbPath = installation.dbPath("layout");
+        tracef("LayoutDB: %s", dbPath);
+        auto flags = installation.mutability == Mutability.ReadWrite
+            ? DatabaseFlags.CreateIfNotExists : DatabaseFlags.ReadOnly;
+
+        /* We have no DB. */
+        if (!dbPath.exists && installation.mutability == Mutability.ReadOnly)
+        {
+            return cast(LayoutResult) fail(format!"LayoutDB: Cannot find %s"(dbPath));
+        }
+
+        Database.open("lmdb://" ~ dbPath, flags).match!((Database db) {
+            this.db = db;
+        }, (DatabaseError err) { throw new Exception(err.message); });
+
+        if (installation.mutability == Mutability.ReadWrite)
+        {
+            immutable err = db.update((scope tx) => tx.createModel!(Layout));
+            if (!err.isNull)
+            {
+                return cast(LayoutResult) fail(err.message);
+            }
+        }
+        return cast(LayoutResult) Success();
+    }
+
+    /**
+     * Close the DB handles
+     */
+    void close() @safe
+    {
+        if (db !is null)
+        {
+            db.close();
+            db = null;
+        }
+    }
+
+private:
+
+    Database db;
+    Installation installation;
+}
