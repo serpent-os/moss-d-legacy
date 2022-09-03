3213fdf353e3b1a6dbc589ec16466cdb1fba406d
Ikey Doherty <ikey@serpentos.com>
2022-09-03 12:17:02 +0100

    impl: Use new Label for improved UI
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/moss/client/label.d
@@ -0,0 +1,76 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * moss.client.label
+ *
+ * Console-based label
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module moss.client.label;
+
+public import moss.client.renderer;
+public import moss.client.ui : Text, Attribute, Color;
+
+/**
+ * Very simple UI label wrapping a Text() struct
+ */
+public final class Label : Renderable
+{
+    this() @safe
+    {
+
+    }
+
+    /**
+     * Construct a new Label with the given Text
+     */
+    this(Text s) @safe
+    {
+        label = s;
+    }
+
+    /**
+     * Render the label
+     */
+    override string render() @safe
+    {
+        return " " ~ _label.toString;
+    }
+
+    /**
+     * Label property
+     *
+     * Params:
+     *      s = New text
+     */
+    pure @property void label(Text s) @safe
+    {
+        if (s == _label)
+        {
+            return;
+        }
+        _label = s;
+        changed = true;
+    }
+
+    /**
+     * Label property
+     *
+     * Returns: Label Text()
+     */
+    pure @property auto label() @safe @nogc nothrow const
+    {
+        return _label;
+    }
+
+private:
+
+    Text _label;
+}
