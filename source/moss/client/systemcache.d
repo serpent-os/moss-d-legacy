acc0d65b67e5be69749e40bbb235d7914e8acede
Ikey Doherty <ikey@serpentos.com>
2022-09-04 21:06:53 +0100

    systemcache: Add lmdb connection code
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/client/systemcache.d
+++ source/moss/client/systemcache.d
@@ -18,15 +18,35 @@ module moss.client.systemcache;
 public import moss.core.errors;
 public import std.sumtype;
 public import moss.client.installation;
+public import std.stdint : uint64_t;
 
 import moss.db.keyvalue;
 import moss.db.keyvalue.errors;
 import moss.db.keyvalue.orm;
+import std.experimental.logger;
+import std.file : exists;
+import std.string : format;
 
 /**
  * All SystemCache operations return a CacheResult
  */
 public alias CacheResult = Optional!(Success, Failure);
+
+/**
+ * A CacheEntry is a mapping of the unique ID to a ref count
+ */
+public @Model struct CacheEntry
+{
+    /**
+     * Unique (hash) identifier for the asset
+     */
+    @PrimaryKey string id;
+
+    /**
+     * How many times this particular entry has been referenced
+     */
+    uint64_t refCount;
+}
 
 /**
  * The SystemCache is the global disk pool of assets which
@@ -55,9 +75,32 @@ public final class SystemCache
      *
      * Returns: Success or Failure
      */
-    CacheResult open() @safe
+    CacheResult connect() @safe
     {
-        return cast(CacheResult) fail("Not yet implemented");
+        immutable dbPath = installation.dbPath("syscache");
+        tracef("SystemCache: %s", dbPath);
+        auto flags = installation.mutability == Mutability.ReadWrite
+            ? DatabaseFlags.CreateIfNotExists : DatabaseFlags.ReadOnly;
+
+        /* We have no DB. */
+        if (!dbPath.exists && installation.mutability == Mutability.ReadOnly)
+        {
+            return cast(CacheResult) fail(format!"SystemCache: Cannot find %s"(dbPath));
+        }
+
+        Database.open("lmdb://" ~ dbPath, flags).match!((Database db) {
+            this.db = db;
+        }, (DatabaseError err) { throw new Exception(err.message); });
+
+        if (installation.mutability == Mutability.ReadWrite)
+        {
+            immutable err = db.update((scope tx) => tx.createModel!(CacheEntry));
+            if (!err.isNull)
+            {
+                return cast(CacheResult) fail(err.message);
+            }
+        }
+        return cast(CacheResult) Success();
     }
 
     /**
