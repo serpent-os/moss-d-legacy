30b4f9bf63f6ccd9c8e408df0c6f2667a5456dab
Ikey Doherty <ikey@serpentos.com>
2023-04-15 23:49:20 +0100

    cli/install: Rectify formatting
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/moss/client/cli/install.d
+++ source/moss/client/cli/install.d
@@ -96,8 +96,8 @@ import std.file : exists;
             }
             return 1;
         }
-        cl.ui.inform!"The following %d %s will be installed\n"
-            (result.length, result.length == 1 ? "package" : "packages");
+        cl.ui.inform!"The following %d %s will be installed\n"(result.length,
+                result.length == 1 ? "package" : "packages");
         auto newPkgs = result.filter!((p) => !p.installed);
         cl.ui.emitAsColumns(newPkgs);
         cl.ui.inform("");
